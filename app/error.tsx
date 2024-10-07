b61ad0eb7bc4ece1ce4013ced9dc3b7405671693
wlfyzz . <mail+a@thatdevwolfy.lol>
2024-10-07 13:52:12 +0000

    update

--- app/error.tsx
+++ app/error.tsx
@@ -13,12 +13,10 @@ export default function Error({
   }, [error])
 
   return (
-    
-    
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-        
       <h2 className="text-2xl mb-4">Something went wrong!</h2>
-      <p className="mb-4">We&apos;re sorry, but an internal server error occured.</p>
+      <p className="mb-4">We're sorry, but an internal server error occurred.</p>
+
       <button
         onClick={
           () => reset()
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="mb-4">Hmm? you probably followed a broken link</p>
-      <Link 
+      <p className="mb-4">Oops! It seems you've stumbled upon a page that doesn't exist.</p>
+      <p className="mb-4">Please check the URL or try searching for what you're looking for.</p>
+      <Link
         href="/"
         className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
       >
         Go back home
       </Link>
     </div>
-  )
+  );
 }
